4f95598234d63a5a78602fcda3453d8afb1f752c
Mostafa Ahmed <m.a7med82002@gmail.com>
2023-12-21 18:57:23 +0200

    constructing hiddenlayer and applying feedforward

---
+++ Layer.py
@@ -0,0 +1,11 @@
+import Neuron
+
+
+class Layer:
+    neurons = []
+
+    def addNeuron(neuron):
+        neurons.append(neuron)
+
+    def getNeurons():
+        return neurons
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import Layer
 import pandas
 import numpy
 from sklearn.model_selection import train_test_split
@@ -15,3 +16,24 @@ def initialize_weights(input_size, hidden_size, output_size):
     Wih = numpy.random.randn(input_size, hidden_size)
     who = numpy.random.randn(hidden_size, output_size)
     return Wih, who
+
+
+def constructHiddenLayer(trainingInput):
+    weightInput, weightHidden = initialize_weights(4, 3, 1)
+    HiddenLayer = Layer()
+    inputs = []
+    weights = []
+    for value in trainingInput:
+        inputs.append(value)
+        weights.append(weightInput)
+    for i in range (3):
+        neuron = Neuron(inputs, weights) 
+        HiddenLayer.addNeuron(neuron)
+    return HiddenLayer
+
+def feedForwardHiddenLayer(hiddenLayer):
+    neurons = hiddenLayer.getNeurons()
+    outputs = []
+    for neuron in neurons:
+        outputs.append(neuron.sigmoid())
+    return outputs
